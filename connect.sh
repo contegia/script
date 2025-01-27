27fcd458ad5dc6bc0ec8b0efdc4a62fe9e1bef83
contegia <contegia.a@gmail.com>
2025-01-27 21:58:51 +0300

    Add files via upload

---
+++ connect.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+########
+#Created by anton 27.03.2024
+########
+ip_server=$1
+user_server=$2
+user_pass=$3
+port_server=3389
+status_f=/home/anton/script/status_start_rdp.txt
+time_start=$(date -R)
+#######check variable#######
+if [ -z "$ip_server" ]; then
+        echo No input ip server
+        echo Usage: connect.sh 192.168.0.100 Username Password
+        exit 1
+fi
+if [ -z "$user_server" ]; then
+        echo No input username server
+        echo Usage: connect.sh 192.168.0.100 Username Password
+        exit 1
+fi
+if [ -z "$user_pass" ]; then
+        echo No input password user server
+        echo Usage: connect.sh 192.168.0.100 Username Password
+        exit 1
+fi
+function rconnect() {
+        echo Start process server:$ip_server of user: $user_server is:$time_start >> $status_f 
+        xvfb-run xfreerdp /cert-ignore /u:"$user_server" /p:"$user_pass" /v:"$ip_server:$port_server"
+        }
+function rclosed() {
+        sleep 20
+        killall xfreerdp
+        exit
+        }
+rconnect & rclosed
+exit
